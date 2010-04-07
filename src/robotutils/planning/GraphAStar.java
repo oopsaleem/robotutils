e602bf7754b04c5e0cefc669fcfc53f369b1b525
psigen <psigen@91e10a42-3053-0410-a8c5-e3b867f4cfc7>
2010-04-07 23:59:17 +0000

    Should provide a reasonable starting implementation for Graph A*.

--- src/robotutils/planning/GraphAStar.java
+++ src/robotutils/planning/GraphAStar.java
@@ -24,13 +24,59 @@
 
 package robotutils.planning;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+import org.jgrapht.Graph;
+
 /**
  * Implementation of the A* algorithm that solves the planning problem for
  * an arbitrary graph, where movement occurs along directed or undirected edges
- * and use the weights associated with the edges, if provided.
+ * and use the weights associated with the edges, if provided.  The heuristic
+ * function <i>h</i> is abstract and must be defined in a subclass before use.
+ * This is because the heuristic function is typically dependent on some domain
+ * specific content of each vertex.
  *
  * @author Prasanna Velagapudi <psigen@gmail.com>
  */
-public class GraphAStar {
+public abstract class GraphAStar<V, E> extends AStar<V> {
+
+    /**
+     * The map over which A* searches will be performed.
+     */
+    final Graph<V, E> _graph;
+
+    /**
+     * Instantiates A* search over the given graph.
+     * @param map A graph where edge weights represent traversal costs
+     */
+    public GraphAStar(Graph<V, E> graph) {
+        _graph = graph;
+    }
+
+    @Override
+    protected Collection<V> succ(V s) {
+        Set<E> edges = _graph.edgesOf(s);
+        Collection<V> succs = new ArrayList(edges.size());
+
+        for (E edge : edges) {
+            if ( _graph.getEdgeSource(edge).equals(s) ) {
+                succs.add(_graph.getEdgeTarget(edge));
+            }
+        }
+
+        return succs;
+    }
+
+    @Override
+    protected double c(V a, V b) {
+        E edge = _graph.getEdge(a, b);
+
+        if (edge != null) {
+            return _graph.getEdgeWeight(edge);
+        } else {
+            return Double.POSITIVE_INFINITY;
+        }
+    }
 
 }
