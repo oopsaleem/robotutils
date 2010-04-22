780b504603e57a337c631beb26d66368b01007d7
psigen <psigen@91e10a42-3053-0410-a8c5-e3b867f4cfc7>
2010-04-22 23:21:16 +0000

    Slowly fixing and extending BiRRT implementation.

--- src/robotutils/planning/BiRRT.java
+++ src/robotutils/planning/BiRRT.java
@@ -30,59 +30,46 @@ package robotutils.planning;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-
-class Condition {
-    static int ADVANCED;
-    static int TRAPPED;
-    static int REACHED;
-}
-
-abstract class StateSampler<T0, T1> {
-
-    static class StateCommandPair<T0, T1> {
-        T0 state;
-        T1 action;
-        public StateCommandPair() {
-        }
-    }
-
-    public abstract StateCommandPair extend(T0 a, T0 b);
-    public abstract T0 random();
-}
+import java.util.Map;
 
 /**
  *
  * @author Prasanna Velagapudi <pkv@cs.cmu.edu>
  */
-public class BiRRT<State, Action> {
+public abstract class BiRRT<State, Action> {
+    
     public static int DEFAULT_ITERATION_LIMIT = 2000;
+
     public static int TRAPPED = -2;
     public static int ADVANCED = -1;
+    public static int REACHED = 0;
 
-    protected class Node {
-        public Node(State s, Action a, int p) {
-            state = s;
-            parent = p;
-            action = a;
+    protected final class Node {
+        public Node(State state, Action action, int parent) {
+            _state = state;
+            _action = action;
+            _parent = parent;
         }
 
-        public final State state;
-        public final Action action;
-        protected final int parent;
+        public final State _state;
+        public final Action _action;
+        protected final int _parent;
     }
 
-    NodeDistance<State> metric;
-    StateSampler<State, Action> sampler;
+    protected abstract double distance(State a, State b);
+    protected abstract Map.Entry<Action, State> newState(State x, State xNear);
+    protected abstract State randomState();
 
-    protected int getNearestNeighbor(List<Node> t, State x) {
+    protected int nearestNeighbor(List<Node> t, State x) {
         double minDist = Double.MAX_VALUE;
         int minIdx = -1;
+
         Node curNode = null;
         double curDist;
 
         for (int idx = 0; idx < t.size(); idx++) {
             curNode = t.get(idx);
-            curDist = metric.distance(curNode.state, x);
+            curDist = distance(curNode._state, x);
 
             if (curDist < minDist) {
                 minIdx = idx;
@@ -95,40 +82,45 @@ public class BiRRT<State, Action> {
     }
 
     protected int extend(List<Node> t, State x) {
-        int iNear = getNearestNeighbor(t, x);
+        int iNear = nearestNeighbor(t, x);
 
-        StateSampler.StateCommandPair<State, Action> xuNew = sampler.extend(t.get(iNear).state, x);
+        Node xuNew = sampler.extend(t.get(iNear).state, x);
         if (xuNew != null) {
             t.add(new Node(xuNew.state, xuNew.action, iNear));
 
-            if (xuNew.state == x) {
-                return 0;
+            if (xuNew._state.equals(x)) {
+                return REACHED;
             } else {
-                return Condition.ADVANCED;
+                return ADVANCED;
             }
         }
 
-        return Condition.TRAPPED;
+        return TRAPPED;
     }
 
+    /**
+     * Generates a path from two tree lists in a BiRRT.  Assumes that the last
+     * node in the A tree was the one that matched.
+     * @param tA the start tree (rooted at the starting node)
+     * @param tB the goal tree (rooted at the goal node)
+     * @return a list of nodes that connect the two trees.
+     */
     private List<Node> path(List<Node> tA, List<Node> tB) {
-        assert(!tA.isEmpty());
-        assert(!tB.isEmpty());
         
         LinkedList<Node> path = new LinkedList<Node>();
 
         // Add path to starting node
         Node nA = tA.get(tA.size() - 1);
         path.add(nA);
-        while (nA.parent > 0) {
-            nA = tB.get(nA.parent);
+        while (nA._parent > 0) {
+            nA = tB.get(nA._parent);
             path.addFirst(nA);
         }
 
         // Add path to ending node
         Node nB = tB.get(tB.size() - 1);
-        while (nB.parent > 0) {
-            nB = tB.get(nB.parent);
+        while (nB._parent > 0) {
+            nB = tB.get(nB._parent);
             path.addLast(nB);
         }
 
@@ -136,7 +128,6 @@ public class BiRRT<State, Action> {
     }
 
     public List plan(State init, State goal, int iterations) {
-        List<Node> tmp = null;
 
         List<Node> tA = new ArrayList();
         tA.add(new Node(init, null, -1));
@@ -145,17 +136,17 @@ public class BiRRT<State, Action> {
         tB.add(new Node(goal, null, -1));
 
         for (int k = 0; k < iterations; k++) {
-            State xRand = sampler.random();
+            State xRand = randomState();
             State xNew = null;
 
-            if (!(extend(tA, xRand) == Condition.TRAPPED)) {
-                if (extend(tB, xNew) == Condition.REACHED) {
+            if (!(extend(tA, xRand) == TRAPPED)) {
+                if (extend(tB, xNew) == REACHED) {
                     return path(tA, tB);
                 }
             }
 
             // SWAP(Ta, Tb);
-            tmp = tA;
+            List<Node> tmp = tA;
             tA = tB;
             tB = tmp;
         }
