f8c4d8cc6d62f1a77ddb7874646bcfff261a5a59
psigen <psigen@91e10a42-3053-0410-a8c5-e3b867f4cfc7>
2010-11-04 03:41:59 +0000

    Implemented all of the priority queue test operations!

--- trunk/test/robotutils/util/PriorityQueueTest.java
+++ trunk/test/robotutils/util/PriorityQueueTest.java
@@ -24,9 +24,11 @@
 
 package robotutils.util;
 
+import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.Random;
 import java.util.Vector;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -38,6 +40,7 @@ import static org.junit.Assert.*;
 public class PriorityQueueTest {
 
     private static Random _rnd;
+    private static final int NUM_ELEMENTS = 1000;
 
     @BeforeClass
     public static void setUpClass() throws Exception {
@@ -51,19 +54,19 @@ public class PriorityQueueTest {
     public void testAdd() {
         System.out.println("add");
 
-        // Add 100 integers to a list
+        // Add NUM_ELEMENTS integers to a list
         Vector<Integer> numbers = new Vector();
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             numbers.add(x);
 
         // Insert those numbers randomly into the queue
         PriorityQueue<Integer> instance = new PriorityQueue();
         while (!numbers.isEmpty())
             instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
-        assertEquals(100, instance.size());
+        assertEquals(NUM_ELEMENTS, instance.size());
 
         // When we remove the numbers, they should be in order
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             assertEquals(x, instance.poll());
     }
 
@@ -74,20 +77,20 @@ public class PriorityQueueTest {
     public void testPeek() {
         System.out.println("peek");
 
-        // Add 100 integers to a list
+        // Add NUM_ELEMENTS integers to a list
         Vector<Integer> numbers = new Vector();
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             numbers.add(x);
 
         // Insert those numbers randomly into the queue
         PriorityQueue<Integer> instance = new PriorityQueue();
         while (!numbers.isEmpty())
             instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
-        assertEquals(100, instance.size());
+        assertEquals(NUM_ELEMENTS, instance.size());
 
         // When we remove the numbers, they should be in order
         // Also, peeking should not affect the next element
-        for (int x = 0; x < 100; x++) {
+        for (int x = 0; x < NUM_ELEMENTS; x++) {
             assertEquals(x, instance.peek());
             assertEquals(x, instance.peek());
             assertEquals(x, instance.poll());
@@ -101,16 +104,16 @@ public class PriorityQueueTest {
     public void testPoll() {
         System.out.println("poll");
 
-        // Add 100 integers to a list
+        // Add NUM_ELEMENTS integers to a list
         Vector<Integer> numbers = new Vector();
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             numbers.add(x);
         
         // Insert those numbers randomly into the queue
         PriorityQueue<Integer> instance = new PriorityQueue();
         while (!numbers.isEmpty())
             instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
-        assertEquals(100, instance.size());
+        assertEquals(NUM_ELEMENTS, instance.size());
 
         // When we remove the numbers, they should be in order
         // Also, polling should affect the next element
@@ -127,43 +130,43 @@ public class PriorityQueueTest {
     public void testRemove() {
         System.out.println("remove");
 
-        // Add 100 integers to a list
+        // Add NUM_ELEMENTS integers to a list
         Vector<Integer> numbers = new Vector();
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             numbers.add(x);
 
         // Insert those numbers randomly into the queue
         PriorityQueue<Integer> instance = new PriorityQueue();
         while (!numbers.isEmpty())
             instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
-        assertEquals(100, instance.size());
+        assertEquals(NUM_ELEMENTS, instance.size());
 
-        // Remove odd numbers from 1 - 50
-        for (int x = 1; x < 50; x+=2)
+        // Remove odd numbers from 1 - NUM_ELEMENTS/2
+        for (int x = 1; x < NUM_ELEMENTS/2; x+=2)
             instance.remove(x);
 
-        // When we remove the first 25 numbers, they should only be even
-        for (int x = 0; x < 25; x++)
+        // When we remove the first numbers, they should only be even
+        for (int x = 0; x < NUM_ELEMENTS/4; x++)
             assertEquals(x*2, instance.poll());
-        assertEquals(50, instance.size());
+        assertEquals(NUM_ELEMENTS/2, instance.size());
 
         // Now remove some stuff that isn't there, just for effect
-        for (int x = 0; x < 25; x++)
-            instance.remove(x + 100);
+        for (int x = 0; x < NUM_ELEMENTS/4; x++)
+            instance.remove(x + NUM_ELEMENTS);
 
         // The queue should not have changed from the last test
         // (50 is the next element, and there should be 50 elements total)
-        assertEquals(50, instance.size());
-        assertEquals(50, instance.peek());
+        assertEquals(NUM_ELEMENTS/2, instance.size());
+        assertEquals(NUM_ELEMENTS/2, instance.peek());
 
         // Now remove the even numbers from the remaining queue
-        for (int x = 50; x < 100; x+=2)
+        for (int x = NUM_ELEMENTS/2; x < NUM_ELEMENTS; x+=2)
             instance.remove(x);
-        assertEquals(25, instance.size());
+        assertEquals(NUM_ELEMENTS/4, instance.size());
 
         // When we remove the next 25 numbers, they should only be odd
-        for (int x = 0; x < 25; x++)
-            assertEquals(x*2 + 51, instance.poll());
+        for (int x = 0; x < NUM_ELEMENTS/4; x++)
+            assertEquals(NUM_ELEMENTS/2 + x*2 + 1, instance.poll());
     }
 
     /**
@@ -173,24 +176,24 @@ public class PriorityQueueTest {
     public void testClear() {
         System.out.println("clear");
 
-        // Add 100 integers to a list
+        // Add NUM_ELEMENTS integers to a list
         Vector<Integer> numbers = new Vector();
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             numbers.add(x);
 
         // Insert those numbers randomly into the queue
         PriorityQueue<Integer> instance = new PriorityQueue();
         while (!numbers.isEmpty())
             instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
-        assertEquals(100, instance.size());
+        assertEquals(NUM_ELEMENTS, instance.size());
 
         // Make sure that they all get cleared
         instance.clear();
         assertEquals(0, instance.size());
 
-        // Add 100 larger integers to a list
+        // Add NUM_ELEMENTS larger integers to a list
         numbers.clear();
-        for (int x = 100; x < 200; x++)
+        for (int x = NUM_ELEMENTS; x < 200; x++)
             numbers.add(x);
 
         // Insert those numbers randomly into the queue
@@ -198,7 +201,7 @@ public class PriorityQueueTest {
             instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
 
         // Make sure none of the older smaller integers appear
-        for (int x = 100; x < 200; x++) {
+        for (int x = NUM_ELEMENTS; x < 200; x++) {
             assertEquals(x, instance.peek());
             assertEquals(x, instance.poll());
         }
@@ -211,28 +214,28 @@ public class PriorityQueueTest {
     public void testElements() {
         System.out.println("elements");
 
-        // Add 100 integers to a list
+        // Add NUM_ELEMENTS integers to a list
         Vector<Integer> numbers = new Vector();
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             numbers.add(x);
 
         // Insert those numbers randomly into the queue
         PriorityQueue<Integer> instance = new PriorityQueue();
         while (!numbers.isEmpty())
             instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
-        assertEquals(100, instance.size());
+        assertEquals(NUM_ELEMENTS, instance.size());
 
-        // Add 100 integers to a list
+        // Add NUM_ELEMENTS integers to a list
         numbers.clear();
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             numbers.add(x);
 
         // Check that we get all those numbers out of the enumeration
         while (!instance.isEmpty()) {
             Integer next = instance.poll();
-            assertTrue(numbers.contains(next));
+            assertTrue(numbers.remove(next));
         }
-        assertTrue(numbers.isEmpty());
+        assertTrue("Missed the numbers: " + numbers, numbers.isEmpty());
     }
 
     /**
@@ -242,23 +245,23 @@ public class PriorityQueueTest {
     public void testSize() {
         System.out.println("size");
 
-        // Add 100 integers to a list
+        // Add NUM_ELEMENTS integers to a list
         Vector<Integer> numbers = new Vector();
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             numbers.add(x);
 
         // Insert those numbers randomly into the queue
         PriorityQueue<Integer> instance = new PriorityQueue();
         while (!numbers.isEmpty())
             instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
-        assertEquals(100, instance.size());
+        assertEquals(NUM_ELEMENTS, instance.size());
 
         // Randomly add and remove numbers, check the size of the queue
         int size = instance.size();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < NUM_ELEMENTS; i++) {
             if (_rnd.nextBoolean()) {
                 size++;
-                instance.add(100 + i);
+                instance.add(NUM_ELEMENTS + i);
             } else {
                 size--;
                 instance.poll();
@@ -274,20 +277,20 @@ public class PriorityQueueTest {
     public void testIsEmpty() {
         System.out.println("isEmpty");
 
-        // Add 100 integers to a list
+        // Add NUM_ELEMENTS integers to a list
         Vector<Integer> numbers = new Vector();
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             numbers.add(x);
 
         // Insert those numbers randomly into the queue
         PriorityQueue<Integer> instance = new PriorityQueue();
         while (!numbers.isEmpty())
             instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
-        assertEquals(100, instance.size());
+        assertEquals(NUM_ELEMENTS, instance.size());
         assertFalse(instance.isEmpty());
 
         // Remove all the numbers
-        for (int x = 0; x < 100; x++)
+        for (int x = 0; x < NUM_ELEMENTS; x++)
             instance.remove(x);
         assertTrue(instance.isEmpty());
     }
@@ -298,11 +301,39 @@ public class PriorityQueueTest {
     @Test
     public void testUpdate() {
         System.out.println("update");
-        Object x = null;
-        PriorityQueue instance = null;
-        instance.update(x);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        // Add NUM_ELEMENTS numbers to a list in an encapsulated datatype
+        Vector<AtomicInteger> numbers = new Vector();
+        for (int x = 0; x < NUM_ELEMENTS; x++)
+            numbers.add(new AtomicInteger(x));
+
+        // Create a comparator that orders this datatype
+        PriorityQueue<AtomicInteger> instance = new PriorityQueue(new Comparator<AtomicInteger>() {
+            public int compare(AtomicInteger o1, AtomicInteger o2) {
+                return Integer.signum(o1.get() - o2.get());
+            }
+        });
+
+        // Insert those numbers randomly into the queue
+        for (AtomicInteger number : numbers)
+            instance.add(number);
+        assertEquals(NUM_ELEMENTS, instance.size());
+
+        // Do a handful of random swaps in the queue
+        for (int i = 0; i < 200; i++) {
+            AtomicInteger a = numbers.get(_rnd.nextInt(NUM_ELEMENTS));
+            AtomicInteger b = numbers.get(_rnd.nextInt(NUM_ELEMENTS));
+
+            // AtomicInteger swapping is fun!
+            a.set(b.getAndSet(a.get()));
+            instance.update(a);
+            instance.update(b);
+        }
+
+        // Test that the queue is still ordered correctly
+        for (int x = 0; x < NUM_ELEMENTS; x++)
+            assertEquals(x, instance.poll().get());
+        assertTrue(instance.isEmpty());
     }
 
     /**
@@ -311,26 +342,37 @@ public class PriorityQueueTest {
     @Test
     public void testContains() {
         System.out.println("contains");
-        Object x = null;
-        PriorityQueue instance = null;
-        boolean expResult = false;
-        boolean result = instance.contains(x);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of deleteElement method, of class PriorityQueue.
-     */
-    @Test
-    public void testDeleteElement() {
-        System.out.println("deleteElement");
-        int i = 0;
-        PriorityQueue instance = null;
-        instance.deleteElement(i);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // Add NUM_ELEMENTS integers to a list
+        Vector<Integer> numbers = new Vector();
+        for (int x = 0; x < NUM_ELEMENTS; x++)
+            numbers.add(x);
+
+        // Insert those numbers randomly into the queue
+        PriorityQueue<Integer> instance = new PriorityQueue();
+        while (!numbers.isEmpty())
+            instance.add(numbers.remove(_rnd.nextInt(numbers.size())));
+        assertEquals(NUM_ELEMENTS, instance.size());
+
+        // Add NUM_ELEMENTS integers to a list
+        numbers.clear();
+        for (int x = 0; x < NUM_ELEMENTS; x++)
+            numbers.add(x);
+
+        // Randomly remove them and check that contains works
+        for (int i = 0; i < 50; i++) {
+            // Remove a random number
+            int x = numbers.remove(_rnd.nextInt(numbers.size()));
+
+            assertTrue(instance.contains(x));
+            instance.remove(x);
+            assertFalse(instance.contains(x));
+
+            // Now pop a number off the top of the queue
+            int y = instance.poll();
+            numbers.remove((Integer)y);
+            assertFalse(instance.contains(y));
+        }
     }
 
     /**
@@ -339,24 +381,40 @@ public class PriorityQueueTest {
     @Test
     public void testHeapify() {
         System.out.println("heapify");
-        PriorityQueue instance = null;
+
+        // Add NUM_ELEMENTS numbers to a list in an encapsulated datatype
+        Vector<AtomicInteger> numbers = new Vector();
+        for (int x = 0; x < NUM_ELEMENTS; x++)
+            numbers.add(new AtomicInteger(x));
+
+        // Create a comparator that orders this datatype
+        PriorityQueue<AtomicInteger> instance = new PriorityQueue(new Comparator<AtomicInteger>() {
+            public int compare(AtomicInteger o1, AtomicInteger o2) {
+                return Integer.signum(o1.get() - o2.get());
+            }
+        });
+
+        // Insert those numbers randomly into the queue
+        for (AtomicInteger number : numbers)
+            instance.add(number);
+        assertEquals(NUM_ELEMENTS, instance.size());
+
+        // Do a handful of random swaps in the queue
+        for (int i = 0; i < 200; i++) {
+            AtomicInteger a = numbers.get(_rnd.nextInt(NUM_ELEMENTS));
+            AtomicInteger b = numbers.get(_rnd.nextInt(NUM_ELEMENTS));
+
+            // AtomicInteger swapping is fun!
+            a.set(b.getAndSet(a.get()));
+        }
+
+        // Apply heap reordering
         instance.heapify();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of swap method, of class PriorityQueue.
-     */
-    @Test
-    public void testSwap() {
-        System.out.println("swap");
-        int i = 0;
-        int j = 0;
-        PriorityQueue instance = null;
-        instance.swap(i, j);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        // Test that the queue is still ordered correctly
+        for (int x = 0; x < NUM_ELEMENTS; x++)
+            assertEquals(x, instance.poll().get());
+        assertTrue(instance.isEmpty());
     }
-
+    
 }
